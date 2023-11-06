b4e4902df6b69dfebbd97cada03a4132fd298026
MAHMUDURRAHMANARNOB <mahmudur.rahman2023@gmail.com>
2023-11-07 00:54:05 +0600

    feat: UI update
    > CooursesDesktop Hero section added with tha appbar

--- lib/screens/Dekstop/courses_screen_desktop.dart
+++ lib/screens/Dekstop/courses_screen_desktop.dart
@@ -11,10 +11,75 @@ class _CoursesScreenDesktopState extends State<CoursesScreenDesktop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*appBar: AppBar(
+      appBar: AppBar(
         title: Text('Courses'),
-      ),*/
-      body: Text('Courses Desktop'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        child: Center(
+          child: Card(
+            margin: const EdgeInsets.all(16.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 5.0,
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                    image: AssetImage('assets/images/courses_back.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.5),
+                      // Adjust the opacity level (0.3 means 30% opacity)
+                      BlendMode.dstATop,
+                    )),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Find your desired courses on',
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    'RISHO.GURU',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Explore Courses',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        /*icon: Icon(Icons.search),*/
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/Mobile/courses_screen_mobile.dart
+++ lib/screens/Mobile/courses_screen_mobile.dart
@@ -15,7 +15,54 @@ class _CoursesScreenMobileState extends State<CoursesScreenMobile> {
         title: Text('Courses'),
         automaticallyImplyLeading: false,
       ),
-      body: Text('Courses Mobile'),
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.all(16.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 5.0,
+          child: Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/courses_back.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                const Text(
+                  'Find your desired courses on',
+                  style: TextStyle(fontSize: 24.0, color: Colors.white),
+                ),
+                const Text(
+                  'RISHO.GURU',
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+                const SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Explore Courses',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    /*icon: Icon(Icons.search),*/
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
