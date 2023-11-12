ce8a9062c177fa116603e6ff512a15bb252f9a72
MAHMUDURRAHMANARNOB <mahmudurrahman2023@gmail.com>
2023-11-12 18:30:55 +0600

    feat: Study board content
    Component feature has been added,
     > text component
     > image component
     loading one after another as a listview on the Study_Board

--- lib/screens/Dekstop/study_board_desktop.dart
+++ lib/screens/Dekstop/study_board_desktop.dart
@@ -32,6 +32,7 @@ class _StudyBoardDesktopState extends State<StudyBoardDesktop> {
   ];*/
 
   List<String> _lessonContents = [];
+  List<Widget> _lessonComponents = [];
 
   @override
   Widget build(BuildContext context) {
@@ -89,25 +90,28 @@ class _StudyBoardDesktopState extends State<StudyBoardDesktop> {
                 Expanded(
                   flex: 2,
                   child: Container(
-                    color: Colors.grey[900],
-                    child: Center(
-                      child: Container(
-                        child: Center(
-                          child: Text(
-                            /*_selectedIndices.isNotEmpty &&
-                                    _selectedIndex < _selectedIndices.length &&
-                                    _selectedIndices[_selectedIndex].isNotEmpty
-                                ? _lessonContents[
-                                    _selectedIndices[_selectedIndex].last]
-                                : 'Select a lesson',
-                            style: TextStyle(fontSize: 24.0),*/
-                            _lessonContents.isNotEmpty
-                                ? _lessonContents.join('\n')
-                                : 'Select a lesson',
-                            style: TextStyle(fontSize: 24.0),
+                    color: Colors.grey[800],
+                    child: ListView.builder(
+                      itemCount: _lessonComponents.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        // Replace this with your actual component widget based on your data
+                        return Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 15.0),
+                          padding: EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                            color: AppColors.backgroundColorDark,
+                            borderRadius: BorderRadius.circular(8.0),
                           ),
-                        ),
-                      ),
+                          child: /*Text(
+                            _lessonContents[index],
+                            style:
+                                TextStyle(fontSize: 24.0, color: Colors.black),
+                          ),*/
+                              _lessonComponents[index],
+                        );
+                        /*_lessonComponents[index];*/
+                      },
                     ),
                   ),
                 ),
@@ -136,7 +140,8 @@ class _StudyBoardDesktopState extends State<StudyBoardDesktop> {
                             /*_lessonContents.add(
                               'Lesson ${_selectedIndex + 1}: $inputText + ${DateTime.now()}',*/
                             _lessonContents
-                                .add('$inputText : \n${DateTime.now()}');
+                                .add('$inputText : ${DateTime.now()}');
+                            _lessonComponents.add(generateComponent(inputText));
                           });
                         },
                         child: Text('Send'),
@@ -174,6 +179,162 @@ class _StudyBoardDesktopState extends State<StudyBoardDesktop> {
                 },
               ))
           .toList(),
+    );
+  }
+
+  Widget generateComponent(String inputText) {
+    switch (inputText.toLowerCase()) {
+      case 'image':
+        // Replace 'ImageWidget' with the actual widget you want to display for images
+        return Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    inputText,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.more_horiz,
+                      color: AppColors.secondaryColor,
+                    ),
+                    onPressed: () {
+                      // Implement your logic to show a popup menu here
+                      // For example, use a PopupMenuButton
+                      setState(() {
+                        showPopupMenu(context);
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_1280.jpg",
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        );
+      case 'text':
+        // Replace 'TextWidget' with the actual widget you want to display for text
+        return Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    inputText,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.more_horiz,
+                      color: AppColors.secondaryColor,
+                    ),
+                    onPressed: () {
+                      // Implement your logic to show a popup menu here
+                      // For example, use a PopupMenuButton
+                      setState(() {
+                        showPopupMenu(context);
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor eu augue ut lectus arcu bibendum at varius. Fringilla est ullamcorper eget nulla facilisi. Amet nisl suscipit adipiscing bibendum est ultricies integer. Sed lectus vestibulum mattis ullamcorper velit. Hendrerit dolor magna eget est lorem ipsum dolor. Eget egestas purus viverra accumsan in. At quis risus sed vulputate odio ut. In aliquam sem fringilla ut morbi tincidunt augue interdum velit. Mi ipsum faucibus vitae aliquet nec. Leo in vitae turpis massa sed elementum tempus egestas. Id neque aliquam vestibulum morbi. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Dictumst quisque sagittis purus sit amet. Ipsum dolor sit amet consectetur adipiscing elit ut. Magna sit amet purus gravida quis blandit. Arcu bibendum at varius vel. Nunc sed augue lacus viverra vitae congue.Mauris nunc congue nisi vitae. Convallis convallis tellus id interdum velit laoreet id. Faucibus et molestie ac feugiat sed lectus. Consequat interdum varius sit amet mattis. Pharetra diam sit amet nisl suscipit. Ut aliquam purus sit amet. Pharetra diam sit amet nisl. Scelerisque viverra mauris in aliquam sem. In fermentum et sollicitudin ac orci. Duis convallis convallis tellus id interdum velit. Feugiat in ante metus dictum at tempor. Congue quisque egestas diam in arcu cursus euismod quis. Convallis aenean et tortor at risus viverra adipiscing. Elementum curabitur vitae nunc sed. Nunc sed velit dignissim sodales ut eu sem integer vitae. Diam donec adipiscing tristique risus nec feugiat. Facilisis sed odio morbi quis commodo odio aenean sed. Ornare arcu odio ut sem nulla pharetra diam. Nisl pretium fusce id velit ut.Enim tortor at auctor urna nunc id cursus metus aliquam. Aliquam ut porttitor leo a diam. Velit scelerisque in dictum non consectetur. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Dignissim suspendisse in est ante in nibh mauris. Morbi blandit cursus risus at ultrices mi tempus imperdiet nulla. Quisque non tellus orci ac auctor augue. Eu tincidunt tortor aliquam nulla. Faucibus nisl tincidunt eget nullam non nisi. Amet consectetur adipiscing elit ut aliquam. Ipsum a arcu cursus vitae congue mauris rhoncus. Aenean et tortor at risus. Semper viverra nam libero justo laoreet sit amet cursus sit. Felis bibendum ut tristique et egestas quis ipsum suspendisse."),
+              ),
+            ],
+          ),
+        );
+
+      default:
+        // Replace 'DefaultWidget' with the default widget you want to display
+        return Text(
+          inputText,
+        );
+    }
+  }
+
+  void showPopupMenu(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Container(
+              width: 200,
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildMenuItem(
+                      context, 'Like', Icons.thumb_up_outlined, 'like'),
+                  buildMenuItem(context, 'Ask Question',
+                      Icons.question_answer_outlined, 'askQuestion'),
+                  buildMenuItem(context, 'Play Audio',
+                      Icons.audiotrack_outlined, 'playAudio'),
+                  buildMenuItem(context, 'Translate', Icons.translate_outlined,
+                      'translate'),
+                  buildMenuItem(context, 'Take Exam', Icons.assignment_outlined,
+                      'takeExam'),
+                  buildMenuItem(context, 'Show Essay Question', Icons.subject,
+                      'showEssayQuestion'),
+                  buildMenuItem(context, 'Show Video',
+                      Icons.video_library_outlined, 'showVideo'),
+                  buildMenuItem(
+                      context, 'Dislike', Icons.thumb_down_outlined, 'dislike'),
+                  buildMenuItem(context, 'Mark Inappropriate',
+                      Icons.report_outlined, 'markInappropriate'),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget buildMenuItem(
+      BuildContext context, String text, IconData icon, String value) {
+    return GestureDetector(
+      onTap: () {
+        // Handle the selected menu item here
+        Navigator.pop(context, value);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Icon(icon, color: AppColors.secondaryColor),
+            SizedBox(width: 8),
+            Text(text),
+          ],
+        ),
+      ),
     );
   }
 }
